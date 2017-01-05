e47bca10a0994b19d60cc9d5415452c98c688711
kiang <kiange@gmail.com>
2017-01-05 16:59:06 +0800

    fix tvbs crawler

--- webdata/models/Crawler/TVBS.php
+++ webdata/models/Crawler/TVBS.php
@@ -3,28 +3,24 @@
 class Crawler_TVBS {
 
     public static function crawl($insert_limit) {
-        $urls = array(
-            'http://news.tvbs.com.tw/opencms/system/modules/com.thesys.project.tvbs/pages/scheduler/ranking-news-daily.jsp',
-            'http://news.tvbs.com.tw/opencms/system/modules/com.thesys.project.tvbs/pages/scheduler/ranking-news-weekly.jsp',
-            'http://news.tvbs.com.tw/opencms/system/modules/com.thesys.project.tvbs/pages/scheduler/ranking-news-choice.jsp',
-            'http://news.tvbs.com.tw/opencms/system/modules/com.thesys.project.tvbs/pages/scheduler/ranking-forum.jsp',
-            'http://news.tvbs.com.tw/opencms/system/modules/com.thesys.project.tvbs/pages/scheduler/ranking-video.jsp',
-            'http://news.tvbs.com.tw/opencms/system/modules/com.thesys.project.tvbs/pages/news/ajax-news-time-list.jsp?dataFolder=%2Fnews%2F&date=' . date('Y-m-d', time() - 3600),
-        );
-        foreach (array('photos', 'politics', 'local', 'money', 'life', 'sports', 'entertainment', 'china', 'world', 'tech', 'travel', 'fun') as $type) {
-            $urls[] = 'http://news.tvbs.com.tw/' . $type;
+        $types = array('news', 'politics', 'local', 'life', 'sports',
+            'entertainment', 'china', 'world', 'tech', 'travel', 'fun');
+        $urls = array();
+        foreach ($types as $type) {
+            $urls[] = 'http://news.tvbs.com.tw/news/realtime/' . $type;
         }
 
         $content = '';
         foreach ($urls as $url) {
             $content .= Crawler::getBody($url);
         }
-        preg_match_all('#href="(/opencms/news/.*/news-[0-9]*)/"]*#', $content, $matches);
-        $links = array_unique($matches[1]);
+        preg_match_all('#href=[\'"]/(' . implode('|', $types) . ')/([0-9]*)[\'"]#i', $content, $matches);
+        $links = array();
+        $links = array_unique($matches[0]);
         $insert = $update = 0;
         foreach ($links as $link) {
             $update ++;
-            $link = 'http://news.tvbs.com.tw' . $link;
+            $link = 'http://news.tvbs.com.tw' . substr($link, 6, -1);
             $insert += News::addNews($link, 9);
             if ($insert_limit <= $insert) {
                 break;
@@ -35,32 +31,17 @@ class Crawler_TVBS {
 
     public static function parse($body) {
         $doc = new DOMDocument('1.0', 'UTF-8');
-        $body = str_replace('<meta charest="utf-8">', '<meta charest="utf-8"><meta http-equiv="Content-Type" content="text/html; charset=utf-8">', $body);
-
         @$doc->loadHTML($body);
         $ret = new StdClass;
-        if ($article_dom = $doc->getElementsByTagName('article')->item(0)) {
-            $ret->title = trim($article_dom->getElementsByTagName('h1')->item(0)->nodeValue);
-            $ret->body = Crawler::getTextFromDom($doc->getElementById('news_contents'));
-        }
 
-        if (!$ret->title) {
-            foreach ($doc->getElementsByTagName('div') as $div_dom) {
-                if ($div_dom->getAttribute('class') == 'reandrBox') {
-                    if ($div_dom->getElementsByTagName('h2')->length == 1) {
-                        $ret->title = $div_dom->getElementsByTagName('h2')->item(0)->nodeValue;
-                    }
-                    $ret->body = '';
-                    foreach (array('Update_time', 'textContent') as $class) {
-                        foreach ($div_dom->getElementsByTagName('div') as $sub_div_dom) {
-                            if (in_array($class, explode(' ', $sub_div_dom->getAttribute('class')))) {
-                                $ret->body .= trim(Crawler::getTextFromDom($sub_div_dom)) . "\n";
-                            }
-                        }
-                    }
-                    $ret->body = trim($ret->body);
+        foreach ($doc->getElementsByTagName('div') as $div_dom) {
+            switch ($div_dom->getAttribute('class')) {
+                case 'newsdetail-h2':
+                    $ret->title = trim(Crawler::getTextFromDom($div_dom));
                     break;
-                }
+                case 'newsdetail-content':
+                    $ret->body = trim(Crawler::getTextFromDom($div_dom));
+                    break;
             }
         }
         return $ret;
