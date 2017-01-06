1d02f342afb957383ad3ba09bd73ae0ee8473934
kiang <kiange@gmail.com>
2017-01-06 22:04:58 +0800

    move auth settings to component

--- ui/Controller/AppController.php
+++ ui/Controller/AppController.php
@@ -5,24 +5,25 @@ App::uses('Controller', 'Controller');
 class AppController extends Controller {
 
     public $helpers = array('Html', 'Form', 'Js', 'Session');
-    public $components = array('Acl', 'Auth', 'RequestHandler', 'Session');
-
-    public function beforeFilter() {
-        if (isset($this->Auth)) {
-            $this->Auth->authenticate = array(
+    public $components = array(
+        'Acl',
+        'Auth' => array(
+            'authorize' => array(
+                'Actions' => array(
+                    'userModel' => 'Member',
+                )
+            ),
+            'unauthorizedRedirect' => false,
+            'loginAction' => '/members/login',
+            'loginRedirect' => '/',
+            'authenticate' => array(
                 'Form' => array(
                     'userModel' => 'Member',
                     'scope' => array('Member.user_status' => 'Y'),
                 )
-            );
-            $this->Auth->loginAction = '/members/login';
-            $this->Auth->loginRedirect = '/';
-            $this->Auth->authorize = array(
-                'Actions' => array(
-                    'userModel' => 'Member',
-                )
-            );
-        }
-    }
+            ),
+        ),
+        'RequestHandler',
+        'Session');
 
 }
